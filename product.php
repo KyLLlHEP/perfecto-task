2638f6fee8d030e334294861aa3fe361682ab538
KyLLlHEP <kushnerartur@gmail.com>
2024-02-06 11:31:55 +0000

    fix: rename file to conn and add product file

--- product.php
+++ product.php
@@ -4,19 +4,19 @@ require_once 'connperfecto.php';
 
 try {
   // query
-  $stmt = $pdo->query('SELECT * FROM Supplier');
+  $stmt = $pdo->query('SELECT p.ProductName, p.Price, s.SupplierName 
+                      FROM Product p
+                      JOIN Supplier s ON p.SupplierID = s.SupplierID');
   
   echo '<table>';
-  echo '<tr><th>Supplier ID</th><th>Supplier Name</th><th>Phone Number</th><th>Email</th><th>Website</th></tr>';
+  echo '<tr><th>Product Name</th><th>Price</th><th>Supplier Name</th></tr>';
   
   // get and show
   while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
       echo '<tr>';
-      echo '<td>' . $row['SupplierID'] . '</td>';
+      echo '<td>' . $row['ProductName'] . '</td>';
+      echo '<td>' . $row['Price'] . '</td>';
       echo '<td>' . $row['SupplierName'] . '</td>';
-      echo '<td>' . $row['PhoneNumber'] . '</td>';
-      echo '<td>' . $row['Email'] . '</td>';
-      echo '<td>' . $row['Website'] . '</td>';
       echo '</tr>';
   }
   
